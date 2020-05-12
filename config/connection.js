7da02de01a88ed57276fce9afea3e2405dc69e08
Michael <mikeystoddard@gmail.com>
2020-05-11 18:00:32 -0700

    updated

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,13 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db",
+  user: process.env.db_user || "root",
+  password: process.env.db_password || "password",
+  database: process.env.db_name || "burgers_db",
+  
 });
+
+
 
 connection.connect(function (err) {
   if (err) {
